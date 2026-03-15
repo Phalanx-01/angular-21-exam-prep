879012684cab8c65096c93263b5f664045a8307f
Luke <phalanx.softdev@gmail.com>
2026-03-15 17:18:55 +0100

    Layout components implementation

--- src/app/layout/shell/shell.ts
+++ src/app/layout/shell/shell.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { Header } from '../header/header';
+import { Sidebar } from '../sidebar/sidebar';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-shell',
-  imports: [],
+  imports: [Header, Sidebar, RouterOutlet],
   templateUrl: './shell.html',
   styleUrl: './shell.scss',
 })
